7b6af79bfc01d60eb18eed17bda462c3c7eb2e33
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-10-11 16:53:34 -0600

    all repos display, need to work on test

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -10,20 +10,15 @@ class DashboardController < ApplicationController
 
      response = @conn.get("/user")
 
-     @repos = JSON.parse(response.body, symbolize_names: true)
+     @results = JSON.parse(response.body, symbolize_names: true)
 
 
+     ##queries
 
-    # @users = GithubService.new(params[:user])
-
-
-    # @conn = Faraday.new(url: "https://api.github.com") do |faraday|
-    #   faraday.adapter Faraday.default_adapter
-    # end
-    #
-    # response = @conn.get("/users/#{user}.json")
-    #
-    # @repos = JSON.parse(response.body, symbolize_names: true)
+     repo_response = @conn.get("/user/repos")
+     @repos = JSON.parse(repo_response.body, symbolize_names: true)
   end
+
+
 
 end
--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -3,18 +3,18 @@ class GithubService
 
   def initialize(user)
     @user = user
+    @conn = Faraday.new(url: "https://api.github.com") do |faraday|
+      faraday.adapter Faraday.default_adapter
+     end
   end
 
 
   def self.find_user_repos
-    binding.pry
-    @conn = Faraday.new(url: "https://api.github.com") do |faraday|
-      faraday.adapter Faraday.default_adapter
-     end
-
-     response = @conn.get("/users/#{user}/repos.json")
+     response = @conn.get("/user/repos")
 
      @repos = JSON.parse(response.body, symbolize_names: true)
+
+
   end
 
 end
