49e8c258b7d379d50f4d17dd2ff16c1ebd627e6a
Ashish Vaid <vaidashi@Ashishs-MacBook-Air.local>
2017-10-15 11:39:54 -0600

    added test for recent commits

--- spec/features/user_can_see_recent_commits_spec.rb
+++ spec/features/user_can_see_recent_commits_spec.rb
@@ -1,23 +1,22 @@
 
 require 'rails_helper'
 
-feature "User can view their basic info" do
+feature "User can view recent activity" do
   let(:user) { User.new(
                         uid:      '999999',
                         nickname: 'vaidashi',
                         token:    ENV['GITHUB_TOKEN'])
                       }
 
-    it "once they log in" do
-      VCR.use_cassette("find_user_info") do
+  it "specifically recent commits" do
+    VCR.use_cassette("find_recent_commits") do
 
       allow_any_instance_of(ApplicationController).to receive(
       :current_user).and_return(user)
 
       visit '/dashboard'
 
-      expect(page).to have_content("starred count: 1")
+      expect(page).to have_content("moving user search info call to model commit from vaidashi/api_curious repo")
     end
   end
-
 end
